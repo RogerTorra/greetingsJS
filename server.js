a27c26b3fb8fd16eb66e6989a784ed9bd8c4d041
Roger Torra <elrogertorra@gmail.com>
2015-01-20 09:55:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Auth,Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 // Force HTTPS on Heroku
